4bb8026930fcdf6e615bc1cf849401ec86cb9c7e
Rakshit Tiwari <59415104+tiwarirakshit@users.noreply.github.com>
2024-03-11 17:31:09 +0530

    Update LocalIp.jsx
    Signed-off-by: Rakshit Tiwari <59415104+tiwarirakshit@users.noreply.github.com>

--- src/components/utils/LocalIp.jsx
+++ src/components/utils/LocalIp.jsx
@@ -5,7 +5,7 @@ const ip =localStorage.getItem('ipaddress');
 const localIp=JSON.parse(ip)
 console.log(ip)
 const BaseAxiosUrl = Axios.create({
-    baseURL: `http://${localIp}:4000/`, // Replace with your actual common IP address
+    baseURL: `http://35.174.208.29:4000/`, // Replace with your actual common IP address
     // You can add other common configurations here
 });
 
